7a0618c091610a33ff18ff6c482ec9b06856fd30
TruptiDadhaniya <trupti@elantechnologies.com>
2012-06-26 10:38:56 +0530

    new demo.

---
+++ ElanDemo/ElanDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ElanDemo
+//
+//  Created by Trupti Dadhaniya on 26/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
---
+++ ElanDemo/ElanDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ElanDemo
+//
+//  Created by Trupti Dadhaniya on 26/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
